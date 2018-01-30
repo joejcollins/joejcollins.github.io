62e664e75e9bd1761114700e3338c33e2c4afaf7
Joe Colllins <joejcollins@gmail.com>
2018-01-30 09:58:26 +0000

    Labels and line on graph

--- _drafts/how-hot-is-my-chilli/04-chillis.r
+++ _drafts/how-hot-is-my-chilli/04-chillis.r
@@ -1,9 +1,9 @@
 chillis <- read.csv(file="./02-chillis.csv", header=TRUE, sep=",")
-plot(chillis$Hotness, chillis$Size.in.cm, 
+plot(chillis$hotness, chillis$size.in.cm, 
      main="Hotness and Size",
      xlab = "Hotness", xlim=c(0, 10),
      ylab = "Length in cm", ylim=c(0, 16),
      col="blue", pch=18)
-abline(lm(chillis$Hotness, chillis$Size.in.cm))
-text(Hotness, Rulings, cex=0.7, pos=4)
-cor.test(chillis$Hotness, chillis$Size.in.cm, method="pearson")
+abline(lm(chillis$hotness ~ chillis$size.in.cm))
+text(chillis$hotness, chillis$size.in.cm, labels = chillis$name, cex=0.7, pos=4)
+cor.test(chillis$hotness, chillis$size.in.cm, method="pearson")
